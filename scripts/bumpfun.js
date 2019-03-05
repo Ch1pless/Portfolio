acb79b72faba843b00cf36f40a0e6289cf1bfa42
Kyle Cochran <42948260+Ch1pless@users.noreply.github.com>
2019-03-05 08:53:31 -0500

    Update bumpfun.js

--- scripts/bumpfun.js
+++ scripts/bumpfun.js
@@ -1,1 +1,5 @@
-document.getElementById(“”)
+let clickDisp = document.getElementById(“clickDisp”),
+clickBtn = document.getElementById(“clickBtn”),
+counter = 0;
+clickDisp.innerHTML=counter;
+clickBtn.addEventListener(“click”, () => {counter++; clickDisp.innerHTML=counter;})
