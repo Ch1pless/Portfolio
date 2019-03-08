5adbfa91efea6ed5339dae3c56a518998abfb30c
Ch1pless <kylecochran123@gmail.com>
2019-03-08 00:13:14 -0500

    cleanup

--- scripts/bumpfun.js
+++
@@ -1,5 +0,0 @@
-let clickDisp = document.getElementById(“clickDisp”),
-clickBtn = document.getElementById(“clickBtn”),
-counter = 0;
-clickDisp.innerHTML=“”+counter+””;
-clickBtn.addEventListener(“click”, () => {counter++; clickDisp.innerHTML=“”+counter+””;})
